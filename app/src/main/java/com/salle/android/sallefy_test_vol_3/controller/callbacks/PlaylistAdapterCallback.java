5182f0e4cf075b5d26fa1d81eb1ccdb8bedd422b
Jcarri14 <j.carrion@students.salle.url.edu>
2020-03-16 02:02:00 +0100

    Create PlaylistAdapterCallback.java

---
+++ app/src/main/java/com/salle/android/sallefy_test_vol_3/controller/callbacks/PlaylistAdapterCallback.java
@@ -0,0 +1,9 @@
+package com.salle.android.sallefy_test_vol_3.controller.callbacks;
+
+import com.salle.android.sallefy_test_vol_3.model.Playlist;
+
+public interface PlaylistAdapterCallback {
+
+    void onPlaylistClick(Playlist playlist);
+
+}
