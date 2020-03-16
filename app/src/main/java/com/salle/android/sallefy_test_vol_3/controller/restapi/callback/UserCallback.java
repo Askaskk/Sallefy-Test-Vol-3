a1621c72d16563b8312c74931b9d38097aabb098
Jcarri14 <j.carrion@students.salle.url.edu>
2020-03-16 12:37:44 +0100

    Update UserCallback.java

--- app/src/main/java/com/salle/android/sallefy_test_vol_3/controller/restapi/callback/UserCallback.java
+++ app/src/main/java/com/salle/android/sallefy_test_vol_3/controller/restapi/callback/UserCallback.java
@@ -3,10 +3,14 @@ package com.salle.android.sallefy_test_vol_3.controller.restapi.callback;
 import com.salle.android.sallefy_test_vol_3.model.User;
 import com.salle.android.sallefy_test_vol_3.model.UserToken;
 
+import java.util.List;
+
 public interface UserCallback extends FailureCallback {
     void onLoginSuccess(UserToken userToken);
     void onLoginFailure(Throwable throwable);
     void onRegisterSuccess();
     void onRegisterFailure(Throwable throwable);
     void onUserInfoReceived(User userData);
+    void onUsersReceived(List<User> users);
+    void onUsersFailure(Throwable throwable);
 }
