a40093ec7797288759d8c03e63876c1e40f22199
Jcarri14 <j.carrion@students.salle.url.edu>
2020-03-16 02:01:49 +0100

    Update MusicService.java

--- app/src/main/java/com/salle/android/sallefy_test_vol_3/controller/music/MusicService.java
+++ app/src/main/java/com/salle/android/sallefy_test_vol_3/controller/music/MusicService.java
@@ -9,7 +9,10 @@ import android.os.Binder;
 import android.os.IBinder;
 import android.util.Log;
 
+import com.salle.android.sallefy_test_vol_3.model.Track;
 import com.salle.android.sallefy_test_vol_3.utils.Constants;
+
+import java.util.ArrayList;
 
 
 public class MusicService extends Service {
@@ -18,6 +21,11 @@ public class MusicService extends Service {
     private final IBinder mBinder = new MusicBinder();
     private AudioManager audioManager;
     private boolean playingBeforeInterruption = false;
+
+    private ArrayList<Track> mTracks;
+    private int currentTrack = 0;
+
+    private MusicCallback mCallback;
 
     public class MusicBinder extends Binder {
         public MusicService getService(){
@@ -84,18 +92,53 @@ public class MusicService extends Service {
         }
 
     }
-    public void updateSessionMusicData(int offset) {
-        /*int oldIndex = Session.getInstance(getApplicationContext()).getIndex();
-        int size = Session.getInstance(getApplicationContext()).getTracks().size();
-        int newIndex = (oldIndex + offset)%size;
-        Session.getInstance(getApplicationContext()).setIndex(newIndex);
-        Track newTrack = Session.getInstance(getApplicationContext()).getTracks().get(newIndex);
-        Session.getInstance(getApplicationContext()).setTrack(newTrack);*/
+
+    public void playStream(ArrayList<Track> tracks, int currentTrack) {
+
+        if (mediaPlayer != null) {
+            try {
+                mediaPlayer.stop();
+            } catch(Exception e) {
+            }
+            mediaPlayer = null;
+        }
+
+        mTracks = tracks;
+        this.currentTrack = currentTrack;
+        String url = mTracks.get(currentTrack).getUrl();
+
+        mediaPlayer = new MediaPlayer();
+        mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                updateTrack(1);
+            }
+        });
+
+        try {
+            mediaPlayer.setDataSource(url);
+            mediaPlayer.prepare();
+            mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                @Override
+                public void onPrepared(MediaPlayer mp) {
+                    mediaPlayer.start();
+                }
+            });
+        } catch(Exception e) {
+        }
+
+    }
+
+    public int getAudioSession() {
+        return mediaPlayer.getAudioSessionId();
     }
 
     public void updateTrack(int offset) {
-        updateSessionMusicData(offset);
-        String newUrl = "";//Session.getInstance(getApplicationContext()).getTrack().getUrl();
+        currentTrack = ((currentTrack+offset)%(mTracks.size()));
+        currentTrack = currentTrack >= mTracks.size() ? 0:currentTrack;
+
+        String newUrl = mTracks.get(currentTrack).getUrl();
         try {
             mediaPlayer.reset();
             mediaPlayer.setDataSource(newUrl);
@@ -105,7 +148,7 @@ public class MusicService extends Service {
         }
     }
 
-    private void pausePlayer() {
+    public void pausePlayer() {
         try {
             mediaPlayer.pause();
            // showNotification();
@@ -138,6 +181,7 @@ public class MusicService extends Service {
     public boolean isPlaying() {
         return mediaPlayer != null && mediaPlayer.isPlaying();
     }
+
     // audio focus section
     public void getAudioFocusAndPlay () {
         audioManager = (AudioManager) this.getBaseContext().getSystemService(Context.AUDIO_SERVICE);
@@ -156,7 +200,7 @@ public class MusicService extends Service {
         }
     }
 
-    public int getCurrrentDuration() {
+    public int getCurrrentPosition() {
         try {
             if (mediaPlayer != null) {
                 return mediaPlayer.getCurrentPosition();
